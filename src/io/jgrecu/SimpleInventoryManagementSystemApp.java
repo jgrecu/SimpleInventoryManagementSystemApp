20c73146212c34dba97ae2349ff95a6596feae2a
Jeremy Grecu <jeremy.grecu@sap.com>
2023-06-29 17:24:57 +0100

    Updated SimpleInventoryManagementSystemApp

--- src/io/jgrecu/SimpleInventoryManagementSystemApp.java
+++ src/io/jgrecu/SimpleInventoryManagementSystemApp.java
@@ -2,13 +2,16 @@ package io.jgrecu;
 
 import io.jgrecu.domain.Product;
 import io.jgrecu.service.FileReaderService;
+import io.jgrecu.service.ReportGenerator;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class SimpleInventoryManagementSystemApp {
     public static void main(String[] args) {
         FileReaderService fileReaderService = FileReaderService.getInstance();
-        List<Product> productList = fileReaderService.readProductsFromFile("sample-data.csv");
-        productList.forEach(System.out::println);
+        ArrayList<Product> productList = fileReaderService.readProductsFromFile("sample-data.csv");
+
+        ReportGenerator reportGenerator = ReportGenerator.getInstance();
+        reportGenerator.generateLowInventoryReport(productList);
     }
 }
