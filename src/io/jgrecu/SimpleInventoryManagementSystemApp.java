1b41416ed2eedf8dfecc0bd7ac32d82da49ca886
Jeremy Grecu <jeremy.grecu@sap.com>
2023-06-29 17:01:06 +0100

    Added simple implementation in main

--- src/io/jgrecu/SimpleInventoryManagementSystemApp.java
+++ src/io/jgrecu/SimpleInventoryManagementSystemApp.java
@@ -1,7 +1,14 @@
 package io.jgrecu;
 
+import io.jgrecu.domain.Product;
+import io.jgrecu.service.FileReaderService;
+
+import java.util.List;
+
 public class SimpleInventoryManagementSystemApp {
     public static void main(String[] args) {
-
+        FileReaderService fileReaderService = FileReaderService.getInstance();
+        List<Product> productList = fileReaderService.readProductsFromFile("sample-data.csv");
+        productList.forEach(System.out::println);
     }
 }
